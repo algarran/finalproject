c9ef2c8066d0e43c42f01c686433688f9208fb02
Michael Baska <mikebaska1@gmail.com>
2020-05-11 16:22:23 -0500

    added the villager seeder

--- mysql-files/villagersDB.sql
+++ mysql-files/villagersDB.sql
@@ -4,20 +4,13 @@ CREATE DATABASE villagers_db;
 USE villagers_db;
 
 CREATE TABLE villagers (
+	id INTEGER AUTO_INCREMENT NOT NULL,
 	villager_name varchar(255) NOT NULL,
   villager_species varchar(255) NOT NULL,
+  villager_gender varchar(255) NOT NULL,
   villager_personality varchar(255) NOT NULL,
-  villager_birthday datetime NOT NULL,
+  villager_birthday date NOT NULL,
   villager_style_aleph varchar(255) NOT NULL,
   villager_style_beth varchar(255),
-  id INTEGER NOT NULL,
   PRIMARY KEY (id)
 );
-
-LOAD DATA INFILE 'C:\Users\Michael%20Baska\bootcamp\homework\finalproject\acnhVillagers.csv' 
-INTO TABLE villagers 
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
-
-SELECT * FROM villagers;
