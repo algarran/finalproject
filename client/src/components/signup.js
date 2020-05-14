827cee7f9a26e2644edb1ebdb7db2d166eb09fe0
Vandana Sivadasan <sivadasanvandana62@gmail.com>
2020-05-14 15:28:10 -0500

    added login&signup js files

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -2,36 +2,44 @@ import React from 'react';
 
 function Signup () {
     return (
-
-   <div>
-      <div class="container">
+  
+  <div>
+  <div class="container">
     <div class="notification">
-      <h1>Login Form</h1>
+      <h1>Signup Form</h1>
     </div>
   </div>
 
   <br/>
-  <div class="container">
-    <div class="startbar" action="/action_page.php">
+  
+  <div className="container">
+    <div className="startbar" action="/action_page.php">
       <label for="email">Email:</label>
       <input type="email" id="email" placeholder="Enter email" name="email"/>
       <label for="pwd">Password:</label>
       <input type="password" id="pwd" placeholder="Enter password" name="pswd"/>
       <button type="submit">Submit</button>
-      Or Signup <a href="calendar.html">here
-      </a>
-      </div>
+      Or Login <a href="/">here
+        </a>
+      
+    </div>
   </div>
-  <br/>
-  <br/>
 
+  <br/>
+  <br/>
+ 
   <div>
     <img src="images/_4REuQH4_400x400.jpg" alt="animalcrossing" class="center"/>
   </div>
 
   <br/>
   <br/>
+
+
+
   <br/>
   </div>
-    );
-  }
+
+   );
+}
+ 
