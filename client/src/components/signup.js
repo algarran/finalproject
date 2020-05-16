cd0bca4015088dd8ce7ff1d4a33dc89f55fc7c00
Nick Algarra <algarran24@gmail.com>
2020-05-16 11:52:12 -0500

    routing pages, and setting up App.js file

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -1,45 +1,46 @@
 import React from 'react';
 
-function Signup () {
-    return (
-  
-  <div>
-  <div class="container">
-    <div class="notification">
-      <h1>Signup Form</h1>
-    </div>
-  </div>
+function Signup() {
+  return (
 
-  <br/>
-  
-  <div className="container">
-    <div className="startbar" action="/action_page.php">
-      <label for="email">Email:</label>
-      <input type="email" id="email" placeholder="Enter email" name="email"/>
-      <label for="pwd">Password:</label>
-      <input type="password" id="pwd" placeholder="Enter password" name="pswd"/>
-      <button type="submit">Submit</button>
+    <div>
+      <div class="container">
+        <div class="notification">
+          <h1>Signup Form</h1>
+        </div>
+      </div>
+
+      <br />
+
+      <div className="container">
+        <div className="startbar" action="/action_page.php">
+          <label for="email">Email:</label>
+          <input type="email" id="email" placeholder="Enter email" name="email" />
+          <label for="pwd">Password:</label>
+          <input type="password" id="pwd" placeholder="Enter password" name="pswd" />
+          <button type="submit">Submit</button>
       Or Login <a href="/">here
         </a>
-      
+
+        </div>
+      </div>
+
+      <br />
+      <br />
+
+      <div>
+        <img src="images/_4REuQH4_400x400.jpg" alt="animalcrossing" class="center" />
+      </div>
+
+      <br />
+      <br />
+
+
+
+      <br />
     </div>
-  </div>
 
-  <br/>
-  <br/>
- 
-  <div>
-    <img src="images/_4REuQH4_400x400.jpg" alt="animalcrossing" class="center"/>
-  </div>
-
-  <br/>
-  <br/>
-
-
-
-  <br/>
-  </div>
-
-   );
+  );
 }
- 
+
+export default Signup;
