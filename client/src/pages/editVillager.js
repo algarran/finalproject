58e443e203d76757cc866d1e019e4f6cf827377a
Ashton <ashtonbeaudoin2000@hotmail.com>
2020-05-16 13:35:55 -0500

    started for loop

--- client/src/pages/editVillager.js
+++ client/src/pages/editVillager.js
@@ -1,5 +1,30 @@
+var mysql = require("mysql");
+var express = require("express");
 import React from "react";
 
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port:
+  user: "",
+  password:"",
+  database: "villagers_db"
+});
+
+connection.connect(function(err){
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+
+});
+app.get("/", function (req, res) {
+  connection.query()
+})
 function EditVillager() {
   // insert FOR loop here.
   return (
