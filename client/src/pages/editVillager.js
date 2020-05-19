1d577eea571c0fa3484628a44e33b1693ff163a9
Michael Baska <mikebaska1@gmail.com>
2020-05-18 20:33:42 -0500

    pray this works

--- client/src/pages/editVillager.js
+++ client/src/pages/editVillager.js
@@ -1,45 +1,6 @@
 import React from "react";
-var mysql = require("mysql");
-var express = require("express");
-
-var app = express();
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password:"yourRootPassword",
-  database: "villagers_db"
-});
-
-connection.connect(function(err){
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-
-});
 
 function EditVillager() {
-  app.get("/", function (req, res) {
-    connection.query("SELECT * FROM villagers", function (err, result){
-      if (err) throw err;
-  
-      var html = "<ul>";
-  
-      for (var i = 0; i < result.length; i++){
-        html += "<li><p>" + result[i].villager_name + "</p>";
-        html +="<p>" + result[i].villager_birthday + "</p></li>";
-  
-      }
-      html += "</ul>";
-  
-      var agrippa = document.getElementById("agrippa");
-      agrippa.appendChild(html);
-  
-      });
-  });
   return (
     <div>
       <div>
--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 var express = require("express");
-
+var mysql = require("mysql");
 var app = express();
-var PORT = process.env.PORT || 8080;
+var db = require("./models");
+var PORT = process.env.PORT || 3001;
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password:"yourRootPassword",
+  database: "villagers_db"
+});
+
+connection.connect(function(err){
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+app.get("/api/villagers", function(req, res) {
+  db.Villagers.findAll({}).then(function(dbVillager) {
+    res.json(dbVillager);
+  });
+});
+
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
